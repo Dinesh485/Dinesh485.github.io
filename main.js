b9a24c4bc2244d65c57c4ecf7087bc9c6bb1f1e1
Dinesh485 <ghantasaladinesh@gmail.com>
2020-06-09 15:00:50 +0530

    1000px media query is complete

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
+  if (window.matchMedia('(max-width: 1000px').matches) {
+    $(".menu-icon").click(function () {
+      $(".menu-icon div:nth-child(1)").toggleClass("top");
+      $(".menu-icon div:nth-child(2)").toggleClass("middle");
+      $(".menu-icon div:nth-child(3)").toggleClass("bottom");
+      $("nav ul").toggleClass("path");
+      $("nav ul li a ").css({ "pointer-events": "all" });
+    });
+    $("nav ul a").click(function () {
+      $(".menu-icon div:nth-child(1)").toggleClass("top");
+      $(".menu-icon div:nth-child(2)").toggleClass("middle");
+      $(".menu-icon div:nth-child(3)").toggleClass("bottom");
+      $("nav ul").toggleClass("path");
+      $("nav ul li a ").css({ "pointer-events": "none" });
+    });
+  }
   
-  $('.menu-icon').click( function () {
-    $('.menu-icon div:nth-child(1)').toggleClass('top')
-    $(".menu-icon div:nth-child(2)").toggleClass("middle");
-    $(".menu-icon div:nth-child(3)").toggleClass("bottom");
-    $('nav ul').toggleClass('path');
-    $('nav ul li a ').css({'pointer-events': 'all'})
-  })
-  $('nav ul a').click(function () {
-     $(".menu-icon div:nth-child(1)").toggleClass("top");
-     $(".menu-icon div:nth-child(2)").toggleClass("middle");
-     $(".menu-icon div:nth-child(3)").toggleClass("bottom");
-     $("nav ul").toggleClass("path");
-     $("nav ul li a ").css({ "pointer-events": "none" });
-  })
 })
